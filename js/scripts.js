59e0b6de1c7c2a122f512f98ea17fdfde462ee14
Brooke Fisher <brookenfish1@gmail.com>
2018-11-06 09:08:15 -0500

    Made the font resizer button useable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(function(){
   console.log('DOM is loaded');
 
+  //print the page
+  $('.fa-print').click(function(){
+    window.print();
+  });
+
+  //increase font size
+  $('.fa-font').on('click', function() { // click to increase or decrease
+    var btn = $(this),
+      fontSize = parseInt(window.getComputedStyle(document.body, null).fontSize, 0); // parse the body font size as a number
+    if (btn[0].id === "increase") { // detect the button being clicked
+      fontSize++; // increase the base font size
+    }
+    document.body.style.fontSize = fontSize + 'px'; // set the body font size to the new value
+  });
+
   //building the high chart map
   $.getJSON('js/rates.json', function (data) {
     console.log(data);
